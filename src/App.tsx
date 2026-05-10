92fb2411ce079b0ba07ac9c6455d520c989cd4b0
Salath Joseph <salathjoseph@Salaths-MacBook-Air.local>
2026-05-10 21:32:53 +0530

    ehnancement checkin

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Expenses from './pages/Expenses/Expenses';
 import EmiCalculator from './pages/Emi/EmiCalculator';
